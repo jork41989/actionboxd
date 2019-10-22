50a5892fbddf886ddf3c2efb0ae07d9b42d939b8
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-22 08:39:24 -0400

    Add movies reducer

---
+++ frontend/src/reducers/movies_reducer.js
@@ -0,0 +1,28 @@
+import {
+    RECEIVE_MOVIE, 
+    RECEIVE_MOVIES,
+    RECEIVE_USER_MOVIES
+} from '../actions/movie_actions';
+import merge from 'lodash/merge'
+
+const moviesReducer = (state, action) => {
+    Object.freeze(state);
+    let newState;
+    let movie;
+    switch(action.type){
+        case RECEIVE_MOVIE:
+            movie = action.movie
+            newState = merge({}, state, { [movie.id]: action.movie });
+            return newState;
+        case RECEIVE_MOVIES:
+            newState = merge({}, state, action.movies);
+            return newState; 
+        case RECEIVE_USER_MOVIES:
+            newState = merge({}, state, action.movies);
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default moviesReducer;
