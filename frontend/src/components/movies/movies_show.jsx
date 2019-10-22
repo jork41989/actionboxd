e33e36e871d6b9370ff3350a0c1d23b9c46c6fe6
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-22 12:18:56 -0400

    Add render conditional

--- frontend/src/components/movies/movies_show.jsx
+++ frontend/src/components/movies/movies_show.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default class MoviesShow extends React.Component {
     componentDidMount(){
+        debugger;
         this.props.getMovie(this.props.match.params.movieId)
     }
 
@@ -12,11 +13,15 @@ export default class MoviesShow extends React.Component {
     }
 
     render() {
+        if (!this.props.movie) {
+            return <div>Loading...</div>;
+        }
+
         return (
             <div className="movie-show-container">
-                {this.props.movie.title}
+                {/* {this.props.movie.title}
                 {this.props.movie.year}
-                {this.props.movie.description}
+                {this.props.movie.description} */}
             </div>
         )
     }
