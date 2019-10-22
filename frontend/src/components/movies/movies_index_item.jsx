6d9493c76f34308b3861e1e87072c6bcc3f3dd23
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-22 19:12:28 -0400

    Add photo thumbnails to grid

--- frontend/src/components/movies/movies_index_item.jsx
+++ frontend/src/components/movies/movies_index_item.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export default function MoviesIndexItem({movie}) {
     return (
         <li className="movies-list-item">
+            <img src={movie.poster_url} alt=""/>
             <Link to={`/movies/${movie._id}`}>
                 {movie.title}
             </Link>
