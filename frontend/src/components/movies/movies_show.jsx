274b99401aff621488485d88786f906b21a117fa
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-22 14:14:19 -0400

    Add background image area

--- frontend/src/components/movies/movies_show.jsx
+++ frontend/src/components/movies/movies_show.jsx
@@ -20,6 +20,15 @@ export default class MoviesShow extends React.Component {
 
         return (
             <div className="movie-show-container">
+                <div className="background-image">
+                    <div>
+                        background image here 
+                    </div>
+                    {/* {this.props.movie.backgroundUrl} */}
+                </div>
+
+
+
                 <div className="movie-show-content">
 
                     <div className="movie-show-poster-link-container">
@@ -32,16 +41,28 @@ export default class MoviesShow extends React.Component {
                                 watched stats here
                                 {/* {this.props.movie.watchedIds.length} */}
                             </ul>
+                            <div className="watch-panel">
+                                trailer link here
+                                {/* this.props.movie.trailerUrl */}
+                            </div>
                         </div>
-                        thumbnail here
                     </div>
-                    <div>
 
+                    <div className="movie-show-info">
+                        {/* {this.props.movie.title}
+                        {this.props.movie.year}
+                        {this.props.movie.description} */}
+                        title here 
+                        year here 
+                        description here
                     </div>
-                    {/* {this.props.movie.title}
-                    {this.props.movie.year}
-                    {this.props.movie.description} */}
+
                 </div>
+
+                <div className="temp">
+                    thing to enable scroll/sticky test here
+                </div>
+
             </div>
         )
     }
