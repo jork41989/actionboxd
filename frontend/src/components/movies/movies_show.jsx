b6e05d9991762374d711c4ee911a70ed98569db1
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-22 09:43:42 -0400

    Add photo show component

---
+++ frontend/src/components/movies/movies_show.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default class movies_show extends React.Component {
+    componentDidMount(){
+        this.props.getMovie(this.props.match.params.movieId)
+    }
+
+    componentDidUpdate(prevProps){
+        if (prevProps.match.params.movieId !== this.props.match.params.movieId){
+            this.props.getMovie(this.props.match.params.movieId)
+        }
+    }
+
+    render() {
+        return (
+            <div className="movie-show">
+                {this.props.movie.title}
+                {this.props.movie.year}
+                {this.props.movie.description}
+            </div>
+        )
+    }
+}
