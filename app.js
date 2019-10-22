c36569a7959ba4b0efc99a8c82499951a42df4d1
Jordan Ackerman <jork41989@gmail.com>
2019-10-22 13:37:30 -0400

    prep for heroku

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 
 const users = require("./routes/api/users");
 const movies = require("./routes/api/movies")
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 
 mongoose
