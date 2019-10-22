a563c98cd90b19db8c1b6254f3722e2a40574c9e
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-22 18:41:49 -0400

    Remove user movies

--- frontend/src/actions/movie_actions.js
+++ frontend/src/actions/movie_actions.js
@@ -2,7 +2,6 @@ import * as MoviesApiUtil from '../util/movies_api_util';
 
 export const RECEIVE_MOVIE = "RECEIVE_MOVIE";
 export const RECEIVE_MOVIES = "RECEIVE_MOVIES";
-export const RECEIVE_USER_MOVIES = "RECEIVE_USER_MOVIES";
 
 const receiveMovie = movie => ({
     type: RECEIVE_MOVIE,
@@ -14,11 +13,6 @@ const receiveMovies = movies => ({
     movies
 });
 
-// const receiveUserMovies = movies => ({
-//     type: RECEIVE_USER_MOVIES,
-//     movies
-// });
-
 export const getMovie = id => dispatch => (
     MoviesApiUtil.getMovie(id)
         .then(movie => dispatch(receiveMovie(movie)))
@@ -28,8 +22,3 @@ export const getMovies = () => dispatch => (
     MoviesApiUtil.getMovies()
         .then(movies => dispatch(receiveMovies(movies)))
 )
-
-// export const getUserMovies = (id) => dispatch => (
-//     MoviesApiUtil.getUserMovies(id)
-//         .then(movies => dispatch(receiveUserMovies(movies)))
-// )
