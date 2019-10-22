699a9a7722f2aa2d7dd14291416f9ca247a42efd
Jordan Ackerman <jork41989@gmail.com>
2019-10-22 13:07:06 -0400

    User auth V1.7

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -34,18 +34,20 @@ export const signup = user => dispatch => (
   ))
 );
 
-export const login = user => dispatch => (
-  APIUtil.login(user).then(res => {
+export const login = user => dispatch => {
+
+  return (APIUtil.login(user).then(res => {
     const { token } = res.data; 
     localStorage.setItem('jwtToken', token); 
     APIUtil.setAuthToken(token);
     const decoded = jwt_decode(token);
+    console.log(decoded)
     dispatch(receiveCurrentUser(decoded))
   })
     .catch(err => {
       dispatch(receiveErrors(err.response.data))
-    })
-)
+    }))
+}
 
 
 
