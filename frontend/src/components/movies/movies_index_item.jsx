e9b85872408e85ff0b2b10a6e4fe696a2ff2e25e
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-22 17:36:18 -0400

    Fix link to show page typo

--- frontend/src/components/movies/movies_index_item.jsx
+++ frontend/src/components/movies/movies_index_item.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function MoviesIndexItem({movie}) {
     return (
         <li className="movies-list-item">
-            <Link to={`/movies/${movie.id}`}>
+            <Link to={`/movies/${movie._id}`}>
                 {movie.title}
             </Link>
         </li>
