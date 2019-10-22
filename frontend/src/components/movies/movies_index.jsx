3ed574ed82685909ad11b6579bba834beeff06d4
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-22 09:00:27 -0400

    Add movies index render

--- frontend/src/components/movies/movies_index.jsx
+++ frontend/src/components/movies/movies_index.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import MoviesIndexItem from './movies_index_item';
 
 class MoviesIndex extends React.Component{
     constructor(props){
         super(props)
+    }
 
+    componentDidMount(){
+        this.props.getMovies();
     }
 
     render(){
-
+        let moviesList = this.props.movies.map(movie => 
+                <MoviesIndexItem key={movie.id} movie={movie} />
+            );
+        
+        return(
+            <div className="movies-list-container">
+                {moviesList}
+            </div>
+        );
     }
 }
 
