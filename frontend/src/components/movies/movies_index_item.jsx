81226bf97480aac4ec1997f1c2e67e61d90aca0e
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-22 19:31:19 -0400

    Adjust position of index grid

--- frontend/src/components/movies/movies_index_item.jsx
+++ frontend/src/components/movies/movies_index_item.jsx
@@ -4,16 +4,18 @@ import { Link } from 'react-router-dom';
 export default function MoviesIndexItem({movie}) {
     return (
         <li className="movies-list-item">
-            <div className="index-thumbnail-container">
-                <img 
-                    src={movie.poster_url} 
-                    alt=""
-                    className="index-thumbnail"
-                />
+            <div className="movies-list-item-container">
+                <div className="index-thumbnail-container">
+                    <img 
+                        src={movie.poster_url} 
+                        alt=""
+                        className="index-thumbnail"
+                        />
+                </div>
+                <Link to={`/movies/${movie._id}`} className="index-thumbnail-link">
+                    {movie.title}
+                </Link>
             </div>
-            <Link to={`/movies/${movie._id}`} className="index-thumbnail-link">
-                {movie.title}
-            </Link>
         </li>
     )
 }
