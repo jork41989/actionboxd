f360a7edf02f8502599e12b3f95b8dac215c03da
Jordan Ackerman <jork41989@gmail.com>
2019-10-23 13:43:36 -0400

    inital profile landing setup

---
+++ frontend/src/components/profile/profile_movies_sample_index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ProfileMoviesSampleIndexItem from './profile_movies_sample_index_item';
+
+
+
+class ProfileMoviesSampleIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.hasMovies = this.hasMovies.bind(this)
+  }
+
+  hasMovies (){
+    if (this.props.movies){
+      return(
+        this.props.movies.map(movie => (
+          <ProfileMoviesSampleIndexItem movie={movie} key={movie.id}/>
+        ))
+      )
+    } else {
+      return (
+        <div>Nothing to see here!</div>
+      )
+    }
+  }
+
+  render (){
+    console.log(this.props)
+    return (
+      <div className={'ProfileMoviesSampleIndex'}>
+      {this.hasMovies()}  
+    </div>)
+  }
+
+}
+
+export default ProfileMoviesSampleIndex;
---
+++ frontend/src/components/profile/profile_movies_sample_index_item.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+class ProfileMoviesSampleIndexItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+
+  render (){
+    console.log(this.props.movie)
+    return (
+      <div className={'ProfileMoviesSampleIndexItem'}>
+        <Link to={`/movies/${this.props.movie._id}`} className="index-thumbnail-link">
+        <div className="movies-list-item-container">
+          <div className="index-thumbnail-container">
+            <img
+              src={this.props.movie.poster_url}
+              alt=""
+              className="index-thumbnail"
+            />
+          </div>
+        </div>
+        </Link>
+      </div>
+    )
+  }
+}
+
+
+export default ProfileMoviesSampleIndexItem;
