a47287b9563c90ec4d07ebf3f9689e714bd8fbae
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-23 16:04:47 -0400

    Play correct trailer on movie show page

--- frontend/src/components/movies/trailer_container.js
+++ frontend/src/components/movies/trailer_container.js
@@ -1,20 +1,20 @@
 import { connect } from 'react-redux';
 import { closeModal } from '../../actions/modal_actions';
+import { withRouter } from 'react-router-dom';
 import Trailer from './trailer';
 
 const mapStateToProps = (state, ownProps) => {
-    debugger;
+    let movieId = ownProps.location.pathname.split("/")[2];
     return({
-
+        movie: state.entities.movies[movieId]
     })
 
 }
 
 const mapDispatchToProps = (dispatch) => {
-    debugger;
     return {
         closeModal: () => dispatch(closeModal())
     };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Trailer);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Trailer));
