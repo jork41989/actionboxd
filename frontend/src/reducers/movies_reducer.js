a75b341fa71ad7949897d7908bedc1370991385e
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-23 18:50:40 -0400

    Add receive review to movies reducer

--- frontend/src/reducers/movies_reducer.js
+++ frontend/src/reducers/movies_reducer.js
@@ -3,7 +3,7 @@ import {
     RECEIVE_MOVIES
 } from '../actions/movie_actions';
 import{
-    RECEIVE_MOVIE_REVIEWS
+    RECEIVE_REVIEW
 } from '../actions/review_actions';
 
 import merge from 'lodash/merge'
@@ -14,6 +14,7 @@ const moviesReducer = (state = {}, action) => {
     let movie;
     let moviesArr;
     let moviesObj;
+    let review;
     switch(action.type){
         case RECEIVE_MOVIE:
             movie = action.movie.data
@@ -27,8 +28,12 @@ const moviesReducer = (state = {}, action) => {
             })
             newState = merge({}, state, moviesObj);
             return newState; 
-        // case RECEIVE_MOVIE_REVIEWS:
-            //not sure what movie reviews look like atm 
+        case RECEIVE_REVIEW:
+            review = action.review;
+            movieId = review.movie_id;
+            newState = merge({}, state);
+            movie = newState[movieId];
+            movie.reviews.push(review._id);
         // case for deleting a movie review 
         default:
             return state;
