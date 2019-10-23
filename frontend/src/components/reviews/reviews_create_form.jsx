4e9fc177551c62b61569d2997dc72b82a8ebdd93
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-23 19:33:10 -0400

    add reviews create form

---
+++ frontend/src/components/reviews/reviews_create_form.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+class ReviewsCreateForm extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            text: "",
+            rating: "",
+            date: ""
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+//errors 
+
+    update(field){
+        return e => {
+            this.setState({[field]: e.currentTarget.value})
+        };
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+//this.props.createReview
+    }
+
+
+    render() {
+        return (
+            <div className="reviewsFormContainer">
+                <form action="">
+
+                    <label htmlFor="1">1</label>
+                    <input type="radio" id="1" value="1" />
+
+                    <label htmlFor="2">2</label>
+                    <input type="radio" id="2" value="2" />
+
+                    <label htmlFor="3">3</label>
+                    <input type="radio" id="3" value="3" />
+
+                    <label htmlFor="4">4</label>
+                    <input type="radio" id="4" value="4" />
+
+                    <label htmlFor="5">5</label>
+                    <input type="radio" id="5" value="5" />
+
+                    <textArea  
+                        onChange={this.update("text")} 
+                        value={this.state.text} 
+                        placeholder="Add a review..." 
+                    />
+                </form>    
+            </div>
+        )
+    }
+}
---
+++ frontend/src/components/reviews/reviews_create_form_container.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import ReviewsCreateForm from './reviews_create_form';
+
+
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({
+//errors
+//forms
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewsCreateForm);
