f86c33196fa349b24c37e95e1728d0564bbbab3a
Jordan Ackerman <jork41989@gmail.com>
2019-10-23 11:02:12 -0400

    Base for profile setup

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -9,6 +9,7 @@ class Profile extends React.Component {
         this.state = {
            
         }
+        this.count = this.count.bind(this)
     }
     
     componentDidMount(e) {
@@ -18,25 +19,53 @@ class Profile extends React.Component {
             this.setState({ user: response.user.data });
         })
     }
+    count (){
+        if (this.state.user.watched_movies){
+            return (<p className={'profileHeaderCountsUpper'}>{this.state.user.watched_movies.length}</p>)
+        }else {
+            return (<p className={'profileHeaderCountsUpper'}>0</p>)
+        }
+    }
     
     render() {
         if (this.state.user){
-        
-       
+            
+            
       return ( <div>
           <div className={'profileHeader'}>
-              <div className={'Profile-photo'}></div>
-              <p className={'Profile-username'}>{this.state.user.username}</p>
+              <div className={'profileHeaderUserInfo'}>
+                <div className={'Profile-photo'}></div>
+                <p className={'Profile-username'}>{this.state.user.username}</p>
+              </div>
+              <div className={'profileHeaderMovieCount'}>
+                {this.count()}
+                  <p className={'profileHeaderMovieCountLabel'}>Films</p>
+              </div>
             </div>
       </div>  )
 
         } else {
-            return (<div>
+            
+            return (
+                <div className={'profileMain'}>
                 <div className={'profileHeader'}>
-                    <div className={'Profile-photo'}></div>
-                    <p className={'Profile-username'}>John Wick</p>
+                    <div className={'profileHeaderUserInfo'}>
+                        <div className={'Profile-photo'}></div>
+                        <p className={'Profile-username'}>John Wick</p>
+                    </div>
+                    <div className={'profileHeaderMovieCount'}>
+                        <p className={'profileHeaderCountsUpper'}>0</p>
+                        <p className={'profileHeaderMovieCountLabel'}>Films</p>
+                    </div>
                 </div>
-            </div>)
+                <div>
+                    <div>
+                            <p>RECENTLY WATCHED</p>
+                            <p>ALL </p>
+                    </div>
+                </div>
+            </div>
+            )
         }
     }
 }
