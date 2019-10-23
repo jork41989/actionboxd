56921f33e5acf35951caf96c00981bfb6f21bf5e
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-23 13:37:03 -0400

    Adjust position on different screen sizes

--- frontend/src/components/movies/trailer.jsx
+++ frontend/src/components/movies/trailer.jsx
@@ -6,10 +6,6 @@ class Trailer extends React.Component {
         return (
             <div className="trailer-container">
                     <iframe className="trailer"
-                        // min-width="560" 
-                        // min-height="315" 
-                        // width="100%" 
-                        // height="auto" 
                         src="https://www.youtube.com/embed/t433PEQGErc" 
                         frameborder="0" 
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
