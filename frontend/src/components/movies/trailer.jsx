59408004ac256176015a288cf3a4ed0aefe00026
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-23 11:57:32 -0400

    Add trailer class component

---
+++ frontend/src/components/movies/trailer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './trailer.css'
+
+export default class trailer extends React.Component {    
+    render() {
+        return (
+            <div className="trailer">
+                
+            </div>
+        )
+    }
+}
