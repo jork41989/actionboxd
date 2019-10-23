4829e92d532e53ec6fe7ff68890587bdc8d89ab0
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-23 16:31:53 -0400

    Add close button to trailer modal

--- frontend/src/components/movies/trailer.jsx
+++ frontend/src/components/movies/trailer.jsx
@@ -6,13 +6,17 @@ class Trailer extends React.Component {
     render() {
         return (
             <div className="trailer-container">
-                    <iframe className="trailer"
-                    src="https://www.youtube.com/embed/akX3Is3qBpw" 
-                    src={this.props.movie.trailer_url}
-                        frameBorder="0" 
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                        allowFullscreen>
-                    </iframe>
+                <div 
+                    className="trailer-close-button"
+                    onClick={this.props.closeModal}
+                >X</div>
+                <iframe className="trailer"
+                src="https://www.youtube.com/embed/akX3Is3qBpw" 
+                src={this.props.movie.trailer_url}
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullscreen>
+                </iframe>
             </div>
         )
     }
