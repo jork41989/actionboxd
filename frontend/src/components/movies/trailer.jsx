76044e1b57ccee773854d42979c5997ae4e38288
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-23 14:10:42 -0400

    Comment in trailer url to src for iframe

--- frontend/src/components/movies/trailer.jsx
+++ frontend/src/components/movies/trailer.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import './trailer.css'
 
 class Trailer extends React.Component {    
+    debugger;
+
+    // let trailer_url = this.props.movie.trailer_url;
+
     render() {
         return (
             <div className="trailer-container">
                     <iframe className="trailer"
                     src="https://www.youtube.com/embed/akX3Is3qBpw" 
+                        // src={trailer_url}
                         frameborder="0" 
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
                         allowfullscreen>
--- frontend/src/components/movies/trailer_container.js
+++ frontend/src/components/movies/trailer_container.js
@@ -2,6 +2,14 @@ import { connect } from 'react-redux';
 import { closeModal } from '../../actions/modal_actions';
 import Trailer from './trailer';
 
+const mapStateToProps = (state) => {
+    debugger;
+    return({
+
+    })
+
+}
+
 const mapDispatchToProps = (dispatch) => {
     return {
         closeModal: () => dispatch(closeModal())
