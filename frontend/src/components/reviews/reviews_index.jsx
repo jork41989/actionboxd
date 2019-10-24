9b92d95b1f7277034e668b956fefa1025c499c6c
Jordan Ackerman <jork41989@gmail.com>
2019-10-24 17:56:02 -0400

    modified modal for global use

--- frontend/src/components/reviews/reviews_index.jsx
+++ frontend/src/components/reviews/reviews_index.jsx
@@ -7,7 +7,7 @@ class ReviewsIndex extends React.Component {
     }
 
     render() {
-        debugger;
+       
         if(!this.props.movie.reviews){
             return <div>Loading...</div>
         }
