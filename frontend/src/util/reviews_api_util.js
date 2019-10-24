173128acee97f40a7b18093e91baa48881abea3b
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-24 12:43:52 -0400

    Resolve merge conflicts

--- frontend/src/util/reviews_api_util.js
+++ frontend/src/util/reviews_api_util.js
@@ -13,25 +13,14 @@ export const getReview = (id) => {
 //   return axios.get(`/api/reviews/movies/${id}`)
 // };
 
-<<<<<<< HEAD
 export const writeReview = (data, movieId, userId) => {
   return axios.post(`/api/reviews/movies/${movieId}/${userId}`, data)
-=======
-export const writeReview = (data) => {
-  router.post('/api/reviews/movies/:movie_id/:user_id', data)
->>>>>>> master
 };
 
 // export const deleteReview = (id) => {
 //   return axios.delete(`api/reviews/${id}`) 
 // }
 
-<<<<<<< HEAD
-// export const updateReview = (id) => {
-//   return axios.patch(`api/reviews/${id}`, data)
-// }
-=======
 export const updateReview = (data) => {
   return axios.patch(`api/reviews/${data.id}`, data)
 }
->>>>>>> master
