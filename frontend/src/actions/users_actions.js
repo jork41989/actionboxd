efd7224001c8d027799287407dc1c13809b8c918
Jordan Ackerman <jork41989@gmail.com>
2019-10-24 15:42:01 -0400

    fixed watched component

--- frontend/src/actions/users_actions.js
+++ frontend/src/actions/users_actions.js
@@ -1,7 +1,9 @@
-import { getUser, updateUser } from '../util/user_api_util';
+import { getUser, watchMovie, unWatchMovie } from '../util/user_api_util';
 
 
 export const RECEIVE_SINGLE_USER = 'RECEIVE_SINGLE_USER';
+export const WATCH_MOVIE = 'WATCH_MOVIE'
+export const UNWATCH_MOVIE = 'UNWATCH_MOVIE'
 
 
 
@@ -11,12 +13,28 @@ export const receiveAUser = user => ({
   user
 })
 
+export const watchANewMovie = user => ({
+  type: WATCH_MOVIE,
+  user
+})
+
+export const unwatchAOldMovie = user => ({
+  type: UNWATCH_MOVIE,
+  user
+})
+
+
+
 export const requestSingleUser = userId => (dispatch) => (
   getUser(userId).then(user => dispatch(receiveAUser(user)))
 )
 
 
 
-export const updateAuser = (userId, payload) => (dispatch) => (
-  updateUser(userId, payload).then(user => dispatch(receiveAUser(user)))
+export const watchAMovie = (userId, payload) => (dispatch) => (
+  watchMovie(userId, payload).then(user => dispatch(watchANewMovie(user)))
+)
+
+export const unwatchAMovie = (userId, payload) => (dispatch) => (
+  unWatchMovie(userId, payload).then(user => dispatch(unwatchAOldMovie(user)))
 )
