d0573bb8d5a323f19596dee5ab00a6b89cb0d3ed
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-24 20:01:06 -0400

    Add 4 and 5 stars

--- frontend/src/components/reviews/reviews_index_item.jsx
+++ frontend/src/components/reviews/reviews_index_item.jsx
@@ -7,6 +7,33 @@ class ReviewsIndexItem extends React.Component{
     }
 
     render(){
+        let rating;
+    
+
+
+        if (this.props.review.rating.$numberDecimal){
+            switch (this.props.review.rating.$numberDecimal){
+                case "1.0":
+                    rating = <span className="green1"></span>
+                    break;
+                case "2.0":
+                    rating = <span className="green2"></span>
+                    break;
+                case "3.0": 
+                    rating = <span className="green3"></span>
+                    break;
+                case "4.0": 
+                    rating = <span className="green4"></span>
+                    break;
+                case "5.0":
+                    rating = <span className="green5"></span>
+                    break;
+                default: 
+                    rating = "";
+            }
+        }
+
+        debugger;
         debugger;
         return (
             <div className="review-item-container">
@@ -17,6 +44,7 @@ class ReviewsIndexItem extends React.Component{
                     <div className="review-item-header">
                         <div className="review-item-header-extended">
                             Review by <p className="review-username">{this.props.review.username}</p>{this.props.review.rating.$numberDecimal}
+                            {rating}
                         </div>
                         <div>
                         
