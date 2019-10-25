4278f080fdf7b0cdaa3e4133b8a5a321a5ca1af8
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-25 11:40:55 -0400

    Add radio button to ratings

--- frontend/src/components/reviews/reviews_create_form.jsx
+++ frontend/src/components/reviews/reviews_create_form.jsx
@@ -78,28 +78,29 @@ class ReviewsCreateForm extends React.Component {
                             <div className={'review-stars-3'}></div>
                             <div className={'review-stars-4'}></div>
                             <div className={'review-stars-5'}></div>
-                            <div className="review-stars-color">
-                            {/* <input className="rating-slider" type="range" min="0" max="10" step="1"/>
-                            <div className="rating-slider-render"></div> */}
+                            <div className="review-stars-color"></div>
 
-                            {/* <label htmlFor="1">1</label>
-                            <input onClick={this.update("rating")} type="radio" id="1" value="1.0" />
-
-                            <label htmlFor="2">2</label>
-                            <input onClick={this.update("rating")} type="radio" id="2" value="2.0" />
-
-                            <label htmlFor="3">3</label>
-                            <input onClick={this.update("rating")} type="radio" id="3" value="3.0" />
-
-                            <label htmlFor="4">4</label>
-                            <input onClick={this.update("rating")} type="radio" id="4" value="4.0" />
-
-                            <label htmlFor="5">5</label>
-                            <input onClick={this.update("rating")} type="radio" id="5" value="5.0" /> */}
                             
-                            </div>
+                            
                             
                         </div>
+                        {/* <input className="review-star-radios" type="range" min="0" max="5" step="1"/> */}
+                            <div className="review-star-radios">
+                                
+                                <input onClick={this.update("rating")} name="rating" type="radio" id="1" value="1.0" />
+
+                                
+                                <input onClick={this.update("rating")} name="rating" type="radio" id="2" value="2.0" />
+
+                                
+                                <input onClick={this.update("rating")} name="rating" type="radio" id="3" value="3.0" />
+
+                                    
+                                <input onClick={this.update("rating")} name="rating" type="radio" id="4" value="4.0" />
+
+                                
+                                <input onClick={this.update("rating")} name="rating" type="radio" id="5" value="5.0" />
+                            </div>
 
                         <div className="submit-row">
                             <button className="reviews-submit">Save</button>
