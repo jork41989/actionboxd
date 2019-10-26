b94a48bd9ee166a4acde8d10e4c4e4d5ce4e7298
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-26 15:43:33 -0400

    Add actor get route

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
-//hi
+
 const Movie = require('../../models/Movie');
 
 router.get('/', (req, res) => {
