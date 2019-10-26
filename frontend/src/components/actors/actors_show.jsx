733fd6a770bf75a33007d19a3180414d84c96b0f
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-26 17:35:53 -0400

    Add component did mount to actor show

--- frontend/src/components/actors/actors_show.jsx
+++ frontend/src/components/actors/actors_show.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 class ActorsShow extends React.Component {
+
+    componentDidMount() {
+        this.props.getActor(this.props.match.params.actorId)
+    }
+    
     render() {
         return (
             <div>
--- frontend/src/components/actors/actors_show_container.js
+++ frontend/src/components/actors/actors_show_container.js
@@ -1,12 +1,14 @@
 import { connect } from 'react-redux';
+import { getActor } from '../../actions/actor_actions';
 import ActorsShow from './actors_show';
+
 
 const mapStateToProps = state => ({
 
 })
 
 const mapDispatchToProps = dispatch => ({
-
+    getActor: (id) => dispatch(getActor(id))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ActorsShow);
