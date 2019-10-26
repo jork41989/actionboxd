78de764bca456b7fe691d5c4a7acffff53657d05
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-26 16:59:48 -0400

    Add actors reducer

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -17,7 +17,8 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   Movie.findById(req.params.id)
   .populate({ path: 'reviews', select: '_id user_id movie_id text rating date username' })
-  // .populate({ path: 'actors', select: '_id name' })
+  .populate({ path: 'actors', select: '_id name' })
+  .exec()
     .then(movie => res.json(movie))
     .catch(err => 
       res.status(404).json({movienotfound: 'Movie not found with that id'}))
