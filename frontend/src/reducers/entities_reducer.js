f30981275b6f896fee5205f0abe711dc6c1d21de
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-26 17:00:24 -0400

    Add actors reducer to entities reducer

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import moviesReducer from './movies_reducer';
 import usersReducer from './users_reducer';
 import reviewsReducer from './reviews_reducer';
+import actorsReducer from './actors_reducer';
 
 export default combineReducers({
     users: usersReducer,
     movies: moviesReducer,
-    reviews: reviewsReducer
+    reviews: reviewsReducer,
+    actors: actorsReducer
 })
