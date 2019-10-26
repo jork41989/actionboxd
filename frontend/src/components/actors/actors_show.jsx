cdbae7848564c4ba2d170c7fea4c85f8180b5b2b
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-26 17:18:01 -0400

    Add actors show

---
+++ frontend/src/components/actors/actors_show.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ActorsShow extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ActorsShow;
