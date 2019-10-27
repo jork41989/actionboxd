47e64fd18bd608220660e92a4871e14ee13c3af7
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-27 16:55:54 -0400

    Add grid to reviews

--- frontend/src/components/reviews/index_page_reviews_item.jsx
+++ frontend/src/components/reviews/index_page_reviews_item.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './index_page_review_item.css'
 
 function IndexPageReviewsItem({review}){
     return(
-        <div>{review.text}</div>
+        <div className="index-review-tile">
+            <div className="index-review-tile-header">
+                <img src={review.movie_id.poster_url} alt={`${review.movie_id.title} poster`}/>
+                <div className="index-review-tile-header-info">
+                    {/* <Link to={`/users/`}> */}
+                    <p>{review.username}</p>
+                    {/* </Link> */}
+                    <p>{review.movie_id.title}</p>
+                    <p>{review.rating.$numberDecimal}</p>
+                </div>
+            </div>
+            <div>
+                {review.text}
+            </div>
+        </div>
     )
 }
 
