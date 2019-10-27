69cbf1ffd54a79788a35dcdc8e247d96d188ea56
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-27 17:23:15 -0400

    Add link to movies, users on review index page

--- frontend/src/components/reviews/index_page_reviews_item.jsx
+++ frontend/src/components/reviews/index_page_reviews_item.jsx
@@ -3,23 +3,58 @@ import { Link } from 'react-router-dom';
 import './index_page_review_item.css'
 
 function IndexPageReviewsItem({review}){
+    let rating;
+
+
+    if (review.rating) {
+        switch (review.rating.$numberDecimal) {
+            case "1.0":
+                rating = <span className="index-green1"></span>
+                break;
+            case "2.0":
+                rating = <span className="index-green2"></span>
+                break;
+            case "3.0":
+                rating = <span className="index-green3"></span>
+                break;
+            case "4.0":
+                rating = <span className="index-green4"></span>
+                break;
+            case "5.0":
+                rating = <span className="index-green5"></span>
+                break;
+            default:
+                rating = "";
+        }
+    }
+
     return(
         <div className="index-review-tile">
             <div className="index-review-tile-header">
                 <div className="index-reviews-thumbnail-container">
-                    <img
-                        className="index-reviews-thumbnail" 
-                        src={review.movie_id.poster_url} alt={`${review.movie_id.title} poster`}/>
+                    <Link
+                        className="index-review-user-link"
+                        to={`/movies/${review.movie_id._id}`}>
+                        <img
+                            className="index-reviews-thumbnail"
+                            src={review.movie_id.poster_url} alt={`${review.movie_id.title} poster`} />
+                    </Link>
                 </div>
                 <div className="index-review-tile-header-info">
-                    {/* <Link to={`/users/`}> */}
-                    <p>{review.username}</p>
-                    {/* </Link> */}
-                    <p>{review.movie_id.title}</p>
-                    <p>{review.rating.$numberDecimal}</p>
+                    <Link 
+                        className="index-review-user-link"
+                        to={`/users/${review.user_id}`}>
+                            {review.username}
+                    </Link>
+                    <Link
+                        className="index-review-user-link"
+                        to={`/movies/${review.movie_id._id}`}>
+                    <h1>{review.movie_id.title}</h1>
+                    </Link>
+                    <p>{rating}</p>
                 </div>
             </div>
-            <div>
+            <div className="index-review-text">
                 {review.text}
             </div>
         </div>
