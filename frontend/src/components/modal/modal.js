239a68bf3233b262f2d20bf611f18d883a5f693c
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-27 13:43:49 -0400

    Add different modal background if payload.modal === trailer

--- frontend/src/components/modal/modal.js
+++ frontend/src/components/modal/modal.js
@@ -31,10 +31,12 @@ function Modal ({payload, closeModal}) {
     default:
       return null;
   }
+// debugger;
+  let modalClass = payload.modal === 'trailer' ? "modal-background-trailer" : "modal-background"
 
   return (
-    // <div className="modal-background">
-    <div className="modal-background" onClick={closeModal}> 
+    <div className={modalClass} onClick={closeModal}>
+    {/* // <div className="modal-background" onClick={closeModal}>  */}
       <div className="modal-child" onClick={e => e.stopPropagation()}>
         {component}
       </div>
