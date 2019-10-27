70735570df10b29a60946ed8fe14f02b165c665b
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-26 21:12:21 -0400

    Add styling to movie thumbnail

--- frontend/src/components/actors/actors_movie_index_item.jsx
+++ frontend/src/components/actors/actors_movie_index_item.jsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 class ActorsMovieIndexItem extends React.Component{
     render(){
         return(
+            <div className="actors-movie-thumbnail-overlay">
             <Link to={`/movies/${this.props.movie._id}`}>
-                <img src={this.props.movie.poster_url} alt=""/>
+                <img className="actors-movie-thumbnail" src={this.props.movie.poster_url} alt=""/>
             </Link>
+            </div>
         )
     }
 }
