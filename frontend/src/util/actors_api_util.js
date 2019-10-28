aa78ab29d3c85c1b19ea3f8c69f234dd62d51ca1
Jordan Ackerman <jork41989@gmail.com>
2019-10-28 13:56:03 -0400

    actors form is up

--- frontend/src/util/actors_api_util.js
+++ frontend/src/util/actors_api_util.js
@@ -3,3 +3,9 @@ import axios from 'axios';
 export const getActor = (id) => {
     return axios.get(`/api/actors/${id}`)
 }
+
+
+
+export const newActor = (data) => {
+    return axios.post(`/api/actors/newActor`, data)
+}
