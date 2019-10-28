cba7276e6b957a0c8fc46803c1ac89d07ced4675
Jordan Ackerman <jork41989@gmail.com>
2019-10-27 21:18:30 -0400

    Got to making the movie form container

---
+++ frontend/src/components/movie-form/movie_form_container.js
@@ -0,0 +1,7 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { closeModal } from '../../actions/modal_actions';
+import { newMovieAdd } from '../../actions/movie_actions'
+import MovieForm from './movie_form'
+
+
