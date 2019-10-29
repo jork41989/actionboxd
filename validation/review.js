73dafef7371f7238cefe3cae427a08fcc77ff475
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-28 20:29:38 -0400

    Transform review.rating. to review.rating prior to update submit

--- validation/review.js
+++ validation/review.js
@@ -4,6 +4,8 @@ const validText = require('./valid-text');
 module.exports = function validateReviewInput(data){
   let errors = {};
 
+  debugger;
+
   data.text = validText(data.text) ? data.text : '';
 
   if(!Validator.isLength(data.text, {min: 5, max: 500})){
@@ -14,9 +16,11 @@ module.exports = function validateReviewInput(data){
     errors.text = 'Text field is required'
   }
 
+  debugger;
   if (!Validator.isFloat(data.rating, { min: 1.0, max: 5.0})){
     errors.rating = 'Rating must be between 1 and 5 stars';
   }
+  debugger;
 
   if (Validator.isEmpty(data.rating)){
     errors.text = "Rating can't be blank";
