cff33ee320a653bfa4518c94e0d085c826d8e44d
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-28 20:35:14 -0400

    Adjust review patch route

--- validation/review.js
+++ validation/review.js
@@ -4,8 +4,6 @@ const validText = require('./valid-text');
 module.exports = function validateReviewInput(data){
   let errors = {};
 
-  debugger;
-
   data.text = validText(data.text) ? data.text : '';
 
   if(!Validator.isLength(data.text, {min: 5, max: 500})){
@@ -16,16 +14,15 @@ module.exports = function validateReviewInput(data){
     errors.text = 'Text field is required'
   }
 
-  debugger;
   if (!Validator.isFloat(data.rating, { min: 1.0, max: 5.0})){
     errors.rating = 'Rating must be between 1 and 5 stars';
   }
-  debugger;
+
 
   if (Validator.isEmpty(data.rating)){
     errors.text = "Rating can't be blank";
   }
-
+  
   return {
     errors,
     isValid: Object.keys(errors).length === 0
