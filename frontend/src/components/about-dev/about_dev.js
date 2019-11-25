053120bad041e1c3cab0ea0d3b0fa4029c79b4d3
Jordan Ackerman <jork41989@gmail.com>
2019-11-25 18:22:45 -0500

    About the devs is live

--- frontend/src/components/about-dev/about_dev.js
+++ frontend/src/components/about-dev/about_dev.js
@@ -14,10 +14,22 @@ class AboutDev extends React.Component{
     
       
         <p className="DevHead">Meet The Developers</p>
-        <div>
-          <p>Jordan Ackerman</p>
-          <i class="fab fa-github"> GitHub</i>
-          <i class="fab fa-linkedin"> LinkedIn</i>
+        <div className="devMain">
+          <div className="devIndi">
+            <p className="devName">Jordan Ackerman</p>
+            <a href="https://github.com/jork41989" className="devLink"><i class="fab fa-github devLink"> GitHub</i></a>
+            <a href="https://www.linkedin.com/in/ackermanjordan/" className="devLink"><i class="fab fa-linkedin devLink"> LinkedIn</i></a>
+          </div>
+          <div className="devIndi">
+            <p className="devName">Frida Pulido</p>
+            <a href="https://github.com/FridaPolished" className="devLink"><i class="fab fa-github devLink"> GitHub</i></a>
+            <a href="https://www.linkedin.com/in/frida-pulido/" className="devLink"><i class="fab fa-linkedin devLink"> LinkedIn</i></a>
+          </div>
+          <div className="devIndi">
+            <p className="devName">Gabriel Antonio Barrios Hernandez</p>
+            <a href="https://github.com/gbarrios212" className="devLink"><i class="fab fa-github devLink"> GitHub</i></a>
+            <a href="https://www.linkedin.com/in/gabriel-antonio-barrios" className="devLink"><i class="fab fa-linkedin devLink"> LinkedIn</i></a>
+          </div>
         </div>
       </div>
     </div>)
